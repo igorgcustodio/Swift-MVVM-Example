d8282cf9687aace1f7208cd2a77548d521a6677a
Torres Conrado, Andre Vinicius <andre.conrado.ext@dasa.com.br>
2021-07-21 18:56:49 -0300

    Initial Commit

---
+++ Shared/SwiftRepositoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRepositoriesApp.swift
+//  Shared
+//
+//  Created by Torres Conrado, Andre Vinicius on 21/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRepositoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
